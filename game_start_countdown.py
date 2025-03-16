224ada5089fe2be2e02142832ef3c53fa8ea768d
bdevaney5 <bdevaney@uark.edu>
2025-03-15 19:39:55 -0500

    Update game_start_countdown.py

--- game_start_countdown.py
+++ game_start_countdown.py
@@ -2,5 +2,6 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from player_entry_gui import Player_Entry_GUI as Player_Entry_GUI
 
-def __init__() -> None
-  
+class Game_Start_Countdown:
+  def __init__(self) -> None
+    
