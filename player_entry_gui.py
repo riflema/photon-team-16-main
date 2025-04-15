2c8787bce0c52518bbba97e59f1c00651dc80bf4
Frank Luceri <frankluceripro@gmail.com>
2025-04-14 22:15:53 -0500

    Update player_entry_gui.py
    changed .destroy() to .pack_forget() in start_game() to make it simpler to bring back with button

--- player_entry_gui.py
+++ player_entry_gui.py
@@ -296,10 +296,10 @@ class Player_Entry_GUI:
         print("Start game")
         python_udpserver.start(self.ip)
         python_udpclient.sendMessage("202", self.ip)
-        self.root.nametowidget(".player_entry.teams").destroy()
-        self.root.nametowidget(".player_entry.instructions").destroy()
-        self.root.nametowidget(".player_entry.game_mode").destroy()
-        self.root.nametowidget(".player_entry.top_edit").destroy()
+        self.root.nametowidget(".player_entry.teams").pack_forget()
+        self.root.nametowidget(".player_entry.instructions").pack_forget()
+        self.root.nametowidget(".player_entry.game_mode").pack_forget()
+        self.root.nametowidget(".player_entry.top_edit").pack_forget()
         red_list:List[str] = []
         for player in range(len(self.red_team)):
             red_list.append(self.red_team[player][0].get())
